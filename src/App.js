b5b82a63572fba29ea45adaab00706c69d97f923
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-05-28 20:05:24 -0300

    Realizando desafio proposto

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { View, StyleSheet, SafeAreaView, StatusBar, Image, Text, Linking } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import { render } from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod';
 
 const colorGit = '#010409';
 const colorFontGitHub = '#C9D1D9';
 const colorDarkFontGitHub = '#4F565E';
+const colorBgSquares = '#45B25C';
+const colorHighlightedSquare = '#00C647';
 const imageProfileGithub =
   'https://avatars.githubusercontent.com/u/78429537?v=4';
 const urlToMyGithub = 'https://github.com/LeticiaBarbosa-dev';
@@ -35,6 +38,181 @@ const App = () => {
         <Text accessibilityLabel='Nome: Letícia Barbosa' style={[style.defaultText, style.name]}>Letícia Barbosa</Text>
         <Text accessibilityLabel='Nickname: LetíciaBarbosa-dev' style={[style.defaultText, style.nickname]}>LeticiaBarbosa-dev</Text>
         <Text accessibilityLabel='Descrição: Desenvolvedora FrontEnd Júnior com mais de dois anos de experiência. @leticiapereiraab' style={[style.defaultText, style.description]}>Desenvolvedora FrontEnd Júnior com mais de dois anos de experiência. @leticiapereiraab</Text>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+        </View>
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+        </View>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+        </View>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+        </View>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+        </View>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.highlightedSquare}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+        </View>
+
+        <View style={style.divSquare}>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+          <View style={style.square}></View>
+        </View>
+
         <Pressable onPress={handlePressGoToGithub}>
           <View style={style.button}>
             <Text style={[style.defaultText, style.textButton]}>Open in Github</Text>
@@ -77,6 +255,7 @@ const style = StyleSheet.create({
   description: {
     fontWeight: 'bold',
     fontSize: 14,
+    marginBottom: 10,
   },
   button: {
     marginTop: 20,
@@ -84,10 +263,29 @@ const style = StyleSheet.create({
     borderRadius: 10,
     padding: 20,
   },
+  divSquare: {
+    flexDirection: 'row',
+    marginTop: 3,
+  },
+  square: {
+    height: 15,
+    width: 15,
+    marginLeft: 3,
+    borderRadius: 3,
+    backgroundColor: colorBgSquares,
+    opacity: 0.76,
+  },
   textButton: {
     fontWeight: 'bold',
     fontSize: 16,
   },
+  highlightedSquare: {
+    backgroundColor: colorHighlightedSquare,
+    marginLeft: 3,
+    borderRadius: 3,
+    height: 15,
+    width: 15,
+  }
 });
 
 export default App;
